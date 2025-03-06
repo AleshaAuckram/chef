88ca1ad80e5038620b4602717f4149e2fff70294
AleshaAuckram <al35ha.joyce@gmail.com>
2025-03-06 15:14:04 +1300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from './components/Header.jsx';
 import './App.css'
 import Body from './components/Body.jsx';
+import React from 'react';
 
 export default function App() {
   return(
-    <>
-    <Header />
+
+  <>
+  <Header />
     <Body />
-    
     </>
   )
+
 }
+    
